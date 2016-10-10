0d277b44fe8cb419ba999b7d876235a9265174cc
Syazwan Ahmad <wan5xp@gmail.com>
2016-10-10 12:25:23 +0200

    vendor: symlink to /system/vendor

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -6,6 +6,9 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
+# Enable vendor image symlink
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
 # TWRP
 TW_INCLUDE_CRYPTO := true
 TW_THEME := landscape_hdpi
